dfbb4b567e4a6d651b02b96feb75d4214d9788e9
Andrew Kovalenko <mr.kovalenko.a.o@gmail.com>
2024-06-18 16:32:52 -0700

    make neovide animation less anoying

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,3 +7,11 @@ vim.g.mapleader = ","
 
 -- Show winbar
 vim.opt.winbar = "%=%m %fjj"
+
+-- Minimize neovide animations
+if vim.g.neovide then
+  vim.g.neovide_position_animation_length = 0.2
+  vim.g.neovide_scroll_animation_length = 0.2
+  vim.g.neovide_cursor_animation_length = 0.2
+  vim.g.neovide_cursor_trail_size = 0
+end
